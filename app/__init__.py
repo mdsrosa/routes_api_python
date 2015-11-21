d894a11982698be0873256caca895b2ed1f187f4
Matheus Rosa <matheusdsrosa@gmail.com>
2015-11-21 03:20:24 -0200

    Add config and db initialization to the application

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,12 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+
+# load configuration file
+app.config.from_object('config')
+
+# database initialization
+db = SQLAlchemy(app)
+db.create_all()
