e3351ed783d3ce62e107e6f07867dcdbb562abf0
Matheus Rosa <matheusdsrosa@gmail.com>
2015-11-21 16:45:38 -0200

    Add SQLALCHEMY_TRACK_MODIFICATIONS config in order to prevent warning from showing

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ DEBUG = True
 # Application directory
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
-
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR,
-                                                      'routes_api.db')
+DATABASE = os.path.join(BASE_DIR, 'routes_api.db')
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DATABASE
 SQLALCHEMY_MIGRATION_REPO = os.path.join(BASE_DIR, 'db_repository')
+SQLALCHEMY_TRACK_MODIFICATIONS = True
