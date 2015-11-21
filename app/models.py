dcc880160a2db364286cef214838d93a92ac8fe5
Matheus Rosa <matheusdsrosa@gmail.com>
2015-11-21 03:28:09 -0200

    Add Route model

--- app/models.py
+++ app/models.py
@@ -8,3 +8,22 @@ class Base(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
     updated_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+
+
+class Route(Base):
+
+    __tablename__ = 'routes'
+
+    origin_point = db.Column(db.String(128), nullable=False)
+    destination_point = db.Column(db.String(128), nullable=False)
+    distance = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, origin_point, destination_point, distance):
+        self.origin_point = origin_point
+        self.destination_point = destination_point
+        self.distance = distance
+
+    def __repr__(self):
+        return '<Route <{0}-{1}-{2]>'.format(self.origin_pint,
+                                             self.destination_point,
+                                             self.distance)
