b245c210c8fa5a780e9f677654bc8760726673a2
Matheus Rosa <matheusdsrosa@gmail.com>
2015-11-21 04:56:53 -0200

    Add RouteAPI resource

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.restful import Api
 
@@ -12,6 +12,23 @@ app.config.from_object('config')
 # database initialization
 db = SQLAlchemy(app)
 
-from app.resources import RouteListAPI
+
+# http error handling
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': "Not found"}), 404)
+
+# api error handling
+api_errors = {
+    'NotFound': {
+        'message': 'Route not found',
+        'status': 404
+    }
+}
+
+api.errors = api_errors
+
+from app.resources import RouteListAPI, RouteAPI
 
 api.add_resource(RouteListAPI, '/routes', endpoint='routes')
+api.add_resource(RouteAPI, '/routes/<int:pk>', endpoint='route')
--- app/resources.py
+++ app/resources.py
@@ -1,8 +1,10 @@
+from flask import abort
 from flask.ext.restful import Resource, reqparse, marshal, fields
 from app.models import Route
 
 
 route_fields = {
+    'uri': fields.Url('route'),
     'origin_point': fields.String,
     'destination_point': fields.String,
     'distance': fields.Integer
@@ -26,3 +28,20 @@ class RouteListAPI(Resource):
     def get(self):
         routes = Route.query.all()
         return {'routes': [marshal(route, route_fields) for route in routes]}
+
+
+class RouteAPI(Resource):
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('origin_point', type=str, location='json')
+        self.reqparse.add_argument('destination_point', type=str,
+                                   location='json')
+        self.reqparse.add_argument('distance', type=int, location='json')
+
+        super(RouteAPI, self).__init__()
+
+    def get(self, pk):
+        route = Route.query.get(pk)
+        if not route:
+            abort(404)
+        return {'route': marshal(route, route_fields)}
