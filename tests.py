66a31578605c3e4a6b5bdf2fa616159673a587a9
Matheus Rosa <matheusdsrosa@gmail.com>
2015-11-21 17:10:24 -0200

    Add tests for RouteCalculateCostAPI

--- tests.py
+++ tests.py
@@ -22,7 +22,7 @@ def clean_db(func):
     return inner
 
 
-class RoutesApiTestCase(unittest.TestCase):
+class RouteApiTestCase(unittest.TestCase):
     def setUp(self):
         app.config['TESTING'] = True
         app.config['SQL_ALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(BASE_DIR, 'test.db')
@@ -33,6 +33,8 @@ class RoutesApiTestCase(unittest.TestCase):
         db.session.remove()
         db.drop_all()
 
+
+class RoutesApiTestCase(RouteApiTestCase):
     def test_empty_database(self):
         response = self.app.get('/routes')
         expected = b'{\n    "routes": []\n}\n'
@@ -137,6 +139,113 @@ class RoutesApiTestCase(unittest.TestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data, expected)
+
+
+class RouteCalculateCostApiTestCase(RouteApiTestCase):
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['SQL_ALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(BASE_DIR, 'test.db')
+        self.app = app.test_client()
+        db.create_all()
+
+        # routes
+        self._create_routes()
+
+    def _create_routes(self):
+        # routes
+        routes = [
+            Route(origin_point="A", destination_point="B", distance=10),
+            Route(origin_point="A", destination_point="C", distance=20),
+            Route(origin_point="B", destination_point="D", distance=15),
+            Route(origin_point="C", destination_point="D", distance=30),
+            Route(origin_point="B", destination_point="E", distance=50),
+            Route(origin_point="D", destination_point="E", distance=30)
+        ]
+
+        for route in routes:
+            db.session.add(route)
+
+        db.session.commit()
+
+    def test_calculate_cost(self):
+        data = {
+            "origin_point": "A",
+            "destination_point": "D",
+            "autonomy": 10,
+            "fuel_price": 2.5
+        }
+
+        data = json.dumps(data)
+
+        response = self.app.post('/routes/calculate-cost', data=data, content_type="application/json")
+
+        expected = b'{\n    "cost": 6.25,\n    "path": "A B D"\n}\n'
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, expected)
+
+    def test_calculate_cost_with_invalid_data(self):
+        data = {
+            "origin_point": "A",
+            "destination_point": "D",
+            "autonomy": "ABC",
+            "fuel_price": 2.5
+        }
+
+        data = json.dumps(data)
+
+        response = self.app.post('/routes/calculate-cost', data=data, content_type="application/json")
+        expected = b'{\n    "message": {\n        "autonomy": "Value \'ABC\' for field \'autonomy\' is not a valid integer."\n    }\n}\n'
+
+        self.assertEqual(response.data, expected)
+        self.assertEqual(response.status_code, 400)
+
+    def test_calculate_cost_with_invalid_fuel_price_data(self):
+        data = {
+            "origin_point": "A",
+            "destination_point": "D",
+            "autonomy": 10,
+            "fuel_price": "XYZ"
+        }
+
+        data = json.dumps(data)
+
+        response = self.app.post('/routes/calculate-cost', data=data, content_type="application/json")
+        expected = b'{\n    "message": {\n        "fuel_price": "Value \'XYZ\' for field \'fuel_price\' is not a valid float."\n    }\n}\n'
+
+        self.assertEqual(response.data, expected)
+        self.assertEqual(response.status_code, 400)
+
+    def test_calculate_cost_with_missing_fields(self):
+        data = {
+            "origin_point": "A",
+            "autonomy": 10,
+            "fuel_price": 2.5
+        }
+
+        data = json.dumps(data)
+
+        response = self.app.post('/routes/calculate-cost', data=data, content_type="application/json")
+        expected = b'{\n    "message": {\n        "destination_point": "Missing required parameter in the JSON body"\n    }\n}\n'
+
+        self.assertEqual(response.data, expected)
+        self.assertEqual(response.status_code, 400)
+
+    def test_calculate_cost_with_nonexistent_point(self):
+        data = {
+            "origin_point": "Y",
+            "destination_point": "A",
+            "autonomy": 10,
+            "fuel_price": 2.5
+        }
+
+        data = json.dumps(data)
+
+        response = self.app.post('/routes/calculate-cost', data=data, content_type="application/json")
+        expected = b'{\n    "error": "Origin point \\"Y\\" not found"\n}\n'
+
+        self.assertEqual(response.data, expected)
+        self.assertEqual(response.status_code, 400)
 
 if __name__ == '__main__':
     unittest.main()
