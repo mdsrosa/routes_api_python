d91f12a36e7980111511a6eb94aea2b09a18cb42
Matheus Rosa <matheusdsrosa@gmail.com>
2015-11-21 06:21:47 -0200

    Add "calculate" method to Route model to calculate the cost

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from app import db
+from app.dijkstra import Graph, get_shortest_path
 
 
 class Base(db.Model):
@@ -22,3 +23,26 @@ class Route(Base):
         return '<Route <{0}-{1}-{2}>'.format(self.origin_point,
                                              self.destination_point,
                                              self.distance)
+
+    @classmethod
+    def calculate(cls, origin, destination, autonomy, fuel_price):
+        distance, path = cls._calculate_shortest_path(origin, destination)
+        cost = cls._calculate_cost(distance, autonomy, fuel_price)
+        return cost, ' '.join(path)
+
+    def _calculate_shortest_path(origin, destination):
+        graph = Graph()
+        routes = Route.query.all()
+
+        for route in routes:
+            graph.add_node(route.origin_point)
+
+        for route in routes:
+            graph.add_edge(route.origin_point,
+                           route.destination_point,
+                           route.distance)
+
+        return get_shortest_path(graph, origin, destination)
+
+    def _calculate_cost(distance, autonomy, fuel_price):
+        return distance * fuel_price / autonomy
