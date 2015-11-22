5a4bd773176c765a63662a74ccd7499f51e08e11
Matheus Rosa <matheusdsrosa@gmail.com>
2015-11-22 20:51:19 -0200

    Remove useless BASE_DIR config

--- config.py
+++ config.py
@@ -4,7 +4,6 @@ import os
 class Config(object):
     DEBUG = False
     TESTING = False
-    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     SQLALCHEMY_TRACK_MODIFICATIONS = True
 
