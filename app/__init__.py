7a4c95171f2bfbea742ae31817485be04d2cb522
Matheus Rosa <matheusdsrosa@gmail.com>
2015-11-22 20:59:44 -0200

    Remove unecessary RouteIndex resource and add a index "/" endpoint

--- app/__init__.py
+++ app/__init__.py
@@ -29,10 +29,14 @@ api_errors = {
 
 api.errors = api_errors
 
-from app.resources import RouteIndex, RoutesAPI, RouteAPI, RouteCalculateCostAPI
+from app.resources import RoutesAPI, RouteAPI, RouteCalculateCostAPI
 
-api.add_resource(RouteIndex, '/', endpoint='routes_api_index')
 api.add_resource(RoutesAPI, '/routes', endpoint='routes')
 api.add_resource(RouteAPI, '/routes/<int:pk>', endpoint='route')
 api.add_resource(RouteCalculateCostAPI, '/routes/calculate-cost',
                  endpoint='route_calculate_cost')
+
+
+@app.route("/", methods=['GET'])
+def index():
+    return 'Routes API Python Version :)\n'
