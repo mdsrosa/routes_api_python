bee340782a07a26a28bf89d91798d8091c42d1e1
Matheus Rosa <matheus.rosa@icloud.com>
2022-10-25 09:39:35 +0100

    Use DB_URL set in heroku to avoid sqlalchemy dialect issue

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 class Config(object):
     DEBUG = False
     TESTING = False
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    SQLALCHEMY_DATABASE_URI = os.environ["DB_URL"]
     SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 
